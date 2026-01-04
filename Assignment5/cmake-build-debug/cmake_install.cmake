faede0441c13a0598ddf410181054f351ad115bf
Evelyn2020 <maythulayatp@gmail.com>
2026-01-04 23:07:40 +0630

    Fix assignment5

--- Assignment5/cmake-build-debug/cmake_install.cmake
+++ Assignment5/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Khin May Thu/CLionProjects/Assignment5
+# Install script for directory: C:/Users/Khin May Thu/CLionProjects/CAssignment/Assignment5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Khin May Thu/CLionProjects/Assignment5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Khin May Thu/CLionProjects/CAssignment/Assignment5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Assignment5/main.c
+++ Assignment5/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <conio.h>
 
 int multiplication();
 
@@ -9,10 +8,7 @@ int main() {
     srand(time(NULL));
     printf("Welcome to the Multiplication Game!\n");
     printf("You have 6 seconds for each question.\n");
-    while (1) {
-        if (!multiplication())
-            break;
-    }
+    while (multiplication());
     printf("\nThanks for playing!\n");
     return 0;
 }
@@ -21,21 +17,22 @@ int multiplication() {
     int r1 = rand() % 10;
     int r2 = rand() % 10;
     int answer = r1 * r2;
-    int user_answer = 0;
-    time_t start = time(NULL);
+    int user_answer;
+    time_t start, end;
+
     printf("\nWhat is %d x %d? ", r1, r2);
-    while (difftime(time(NULL), start) < 6) {
-        if (_kbhit()) {
-            scanf("%d", &user_answer);
-            if (user_answer == answer) {
-                printf("Correct!\n");
-                return 1;
-            } else {
-                printf("Wrong! Correct answer: %d\n", answer);
-                return 0;   // stop game
-            }
-        }
+    time(&start);
+    scanf("%d", &user_answer);
+    time(&end);
+    while (difftime(end, start) > 6) {
+        printf("Time's up!\n");
+        return 0;
     }
-    printf("\nTime's up!\n");
-    return 0;
+    if (user_answer == answer) {
+        printf("Correct!\n");
+        return 1;
+    } else {
+        printf("Wrong! Correct answer: %d\n", answer);
+        return 0;
+    }
 }
